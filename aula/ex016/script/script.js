0ed086b7ee2b5a5a438e4f0742714b8dab84d1e8
Rodrigo Andrade <144704436+soadse@users.noreply.github.com>
2024-03-22 14:54:00 -0300

    .

--- aula/ex016/script/script.js
+++ aula/ex016/script/script.js
@@ -1,16 +1,17 @@
 function clicar(){
     var numero = window.document.getElementById('num')
     var num = Number(numero.value)
+    var campo2 = window.document.getElementById('res2')
     var campo = window.document.getElementById('res')
-    campo.style.display = 'block'
     if(num == 0){
         campo.innerText = 'Digite um número válido'
+        campo2.innerText = ''
     } else {
         campo.innerText = ''
-        for(c = 1; c <= 10; c++ ){   
-            var item = document.createElement('option')
-            item.innerHTML += `${c} x ${num} = ${c*num} <br>`  
-            campo.appendChild(item) 
+        campo2.style.fontWeight = 'bolder'
+        campo2.innerText = `Tabuada do ${num}`
+        for(c = 1; c <= 10; c++ ){  
+            campo.innerHTML += `${c} x ${num} = ${c*num} <br>`  
         }
     }
 }
