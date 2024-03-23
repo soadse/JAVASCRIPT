362d15c08b3d4400774e5d84dc00979663f74465
Rodrigo Andrade <144704436+soadse@users.noreply.github.com>
2024-03-23 14:54:05 -0300

    .

--- aula/ex017/script/script.js
+++ aula/ex017/script/script.js
@@ -1,16 +1,57 @@
+var nome, senha
+function pegar(){
+    nome = document.getElementById('nome').value
+    senha = document.getElementById('senha').value
+}
 function mostra(){
-    var nao_mostra = window.document.getElementById('img-nao-mostra')
-    var mostra = window.document.getElementById('img-mostra')
-    var dinheiro = window.document.getElementById('saldo-conta')
+    var n_mostra = window.document.getElementById('n-mostra')
+    var mostra = window.document.getElementById('mostra')
+    var dinheiro = window.document.getElementById('saldo')
     dinheiro.style.display = 'flex'
-    nao_mostra.style.display = 'none'
+    n_mostra.style.display = 'none'
     mostra.style.display = 'flex'   
 }
 function nao_mostra(){
-    var nao_mostra = window.document.getElementById('img-nao-mostra')
-    var mostra = window.document.getElementById('img-mostra')
-    var dinheiro = window.document.getElementById('saldo-conta')
+    var n_mostra = window.document.getElementById('n-mostra')
+    var mostra = window.document.getElementById('mostra')
+    var dinheiro = window.document.getElementById('saldo')
     dinheiro.style.display = 'none'
-    nao_mostra.style.display = 'flex'
+    n_mostra.style.display = 'flex'
     mostra.style.display = 'none'   
+}
+
+function area_deposito(){
+    var area_deposito = window.document.getElementBycl('deposito')
+    var areas = window.document.getElementById('saque')
+    area_deposito.style.background = 'red'
+    areas.style.display = 'none'
+}
+function depositar(){
+    var envio = window.document.getElementById('envio')
+    envio.style.boxShadow = '5px 5px 5px black'
+    var valor = window.document.getElementById('valor')
+    var deposito = window.document.getElementById('dep')
+    var num = Number(deposito.value) 
+    valor.innerText = `${num}`
+}
+
+function area_saque(){
+    var area_saque = window.document.getElementById('saque')
+    area_saque.style.display = 'block'
+    var area = window.document.getElementById('deposito')
+    area.style.display = 'none'
+}
+
+
+
+
+
+
+
+
+
+
+function sair(){
+    var envio = window.document.getElementById('envio')
+    envio.style.boxShadow = '10px 10px 10px rgba(0, 0, 0, 0.5)'
 }
