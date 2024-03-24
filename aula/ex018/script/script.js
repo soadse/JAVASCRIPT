2caf3b60fd117e7b8d1c72ae72803079eb53ec7a
Rodrigo Andrade <144704436+soadse@users.noreply.github.com>
2024-03-23 21:20:20 -0300

    .

--- aula/ex018/script/script.js
+++ aula/ex018/script/script.js
@@ -1,6 +1,12 @@
-var num = [0, 4, 6, 7]
-num[4] = 9
-num.push(4)
-num.length
-num.sort
-console.log(`O vetor tem ${num.length} posições\n${num}`)
+var num = [1, 4, 6, 7, 3, 9]
+let pos = num.indexOf(9)
+console.log(`O vetor tem ${num.length}`)
+console.log(`O valor 9 está na posição ${pos}`)
+
+/*for(pos = 0; pos < num.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}*/
+
+for(var posi in num){
+    console.log(`A posição ${posi} tem o valor ${num[posi]}`)
+}
